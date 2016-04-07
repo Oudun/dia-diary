4440a8a89d6ebc450d37b4470792b70e9ca09f08
oudun@mail.ru <8adagris>
2016-04-07 18:57:57 +0300

    Glucose Details updated

--- DiaDiary/GlucosePageEdit.xaml.cs
+++ DiaDiary/GlucosePageEdit.xaml.cs
@@ -17,7 +17,7 @@ namespace PhoneAppDB {
     
     public partial class GlucosePageEdit : PhoneApplicationPage {
 
-        public GlucoseRecord GlucoseRecord;
+        public GlucoseRecord glucoseRecord;
         
         private AppDataContext dataContext;
 
@@ -31,28 +31,50 @@ namespace PhoneAppDB {
         {
 
             string selectedIndex = "";
-
-            GlucoseRecord gk = new GlucoseRecord();
             
-            if (NavigationContext.QueryString.TryGetValue("selectedItem", out selectedIndex))
-            {
+            if (NavigationContext.QueryString.TryGetValue("selectedItem", out selectedIndex)) {
                 int index = int.Parse(selectedIndex);
                 this.DataContext = this;
                 dataContext = new AppDataContext(AppDataContext.DBConnectionString);
-
-//                var glucoseRecordsInDB = from GlucoseRecord glucoseRecord in dataContext.GlucoseRecordsTable select glucoseRecord;
-//                GlucoseRecords = new ObservableCollection<GlucoseRecord>(glucoseRecordsInDB);
-//                base.OnNavigatedTo(e);
-
                 var item = dataContext.GlucoseRecordsTable.Single(x => x.GlucoseRecordId == index);
-
-                gk = item;
-
-//                gk.GlucoseRecordId = index;
-                LayoutRoot.DataContext = gk;
+                glucoseRecord = item;
+                LayoutRoot.DataContext = glucoseRecord;
             }
 
 
+
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Save_Record(object sender, RoutedEventArgs e) {
+
+            glucoseRecord.GlucoseRecordValue = float.Parse(RecordValue.Text);
+            dataContext.SubmitChanges();
+            NavigationService.Navigate(new Uri("/GlucosePage.xaml", UriKind.Relative));
+
+            //var tag = (sender as Button).Tag;
+            //int t = Convert.ToInt16(tag);
+            //NavigationService.Navigate(new Uri("/GlucosePageEdit.xaml?selectedItem=" + t, UriKind.Relative));
+
+        }
+
+        private void Delete_Record(object sender, RoutedEventArgs e) {
+
+            dataContext.GlucoseRecordsTable.DeleteOnSubmit(glucoseRecord);
+            dataContext.SubmitChanges();
+
+            var tag = (sender as Button).Tag;
+            int t = Convert.ToInt16(tag);
+            NavigationService.Navigate(new Uri("/GlucosePage.xaml", UriKind.Relative));
+
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e) {
+
+            NavigationService.Navigate(new Uri("/GlucosePage.xaml", UriKind.Relative));
 
         }
     
